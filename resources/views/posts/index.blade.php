bf55602e0b5b0bcd2d47ac1f5ccf29561d1f075f
tuluswahyuno <tuluswahyuno@gmail.com>
2021-03-18 19:42:48 +0700

    edit judul

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
             <div class="float-left">
-                <h2>Tutorial CRUD Laravel 8 untuk Pemula - Tulus Wahyuno</h2>
+                <h2>Simple CRUD Laravel 8 - Tulus Wahyuno</h2>
             </div>
             <div class="float-right">
                 <a class="btn btn-success" href="{{ route('posts.create') }}"> Create Post</a>
